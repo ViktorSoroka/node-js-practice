8324aeb6dedfcb7097592a61f425742c5e54effe
Viktor Soroka <Viktor_Soroka@epam.com>
2015-05-26 15:19:23 +0300

    created server

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ var http = require('http'),
     fs = require("fs"),
     mime = require('mime'),
     path = require('path'),
-    url = require('url');
+    url = require('url'),
+    db = require('./fake-db');
 
 var ROOT = __dirname,
     initialPath;
 
 http.createServer(function (req, res) {
-    sendFile(url.parse(req.url).pathname, res);
+    sendFile(url.parse(req.url, true).pathname, res, req);
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000/');
 
 
-function sendFile(filePath, res) {
+function sendFile(filePath, res, req) {
+
     initialPath = filePath;
     try {
         filePath = decodeURIComponent(filePath);
@@ -36,25 +38,121 @@ function sendFile(filePath, res) {
     }
 
     fs.stat(filePath, function (err, stats) {
+        var pattern = initialPath.match(/^\/api\/users(\/(\w+))?/);
+        if (pattern && pattern[0]) {
+            var body = '',
 
-        if (err || !stats.isFile()) {
+                id = pattern[2];
+
+            if (req.method === 'GET') {
+                id = pattern[2];
+                if (id) {
+                    db.getById(id, function (err, data) {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+                        res.end(JSON.stringify(data));
+                    });
+                    return;
+                }
+                db.getCollection(function (err, db) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+                    res.end(JSON.stringify(db));
+                });
+                return;
+            }
+
+            if (req.method === 'POST') {
+                body = '';
+                req
+                    .on('readable', function () {
+                        body += req.read();
+                    })
+                    .on('end', function () {
+                        body = JSON.parse(body);
+                        db.create(body, function () {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json; charset=utf-8');
+                            res.end(JSON.stringify(db));
+                        });
+                    });
+                return;
+            }
+
+            if (req.method === 'PUT') {
+
+                body = '';
+                req
+                    .on('readable', function () {
+                        body += req.read();
+                    })
+                    .on('end', function () {
+                        body = JSON.parse(body);
+                        db.update(body, function () {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json; charset=utf-8');
+                            res.end(JSON.stringify(db));
+                        });
+                    });
+                return;
+            }
+
+            if (req.method === 'DELETE') {
+                db.remove(id, function () {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+                    res.end(JSON.stringify(db));
+                });
+                return;
+            }
+        }
+
+        if (err) {
+            res.statusCode = 404;
+            res.end('Not found');
+            return;
+        }
+
+        if (stats.isFile()) {
             if (initialPath !== '/' && !/^\/public\//.test(initialPath)) {
                 res.statusCode = 403;
                 res.end('access is denied!');
                 return;
             }
-            res.statusCode = 404;
-            res.end('File not found!');
-            return;
-        }
 
-        fs.readFile(filePath, function (err, content) {
-            if (err) throw err;
-
+            var file = fs.createReadStream(filePath);
             var contentType = mime.lookup(filePath);
             res.setHeader('Content-Type', contentType + "; charset=utf-8");
-            res.end(content);
-        });
+            file.pipe(res);
 
-    })
+            file.on('error', function () {
+                res.statusCode = 500;
+                res.end('Server error!');
+                console.log('Server error!');
+            });
+
+            file.on('close', function () {
+                res.statusCode = 200;
+                res.end('ee');
+            });
+
+            file.on('end', function () {
+                res.statusCode = 200;
+                res.end('ee');
+            });
+
+            res.on('close', function () {
+                file.destroy();
+            });
+
+            //another variant of stream
+            //fs.readFile(filePath, function (err, content) {
+            //    if (err) throw err;
+            //
+            //    var contentType = mime.lookup(filePath);
+            //    res.setHeader('Content-Type', contentType + "; charset=utf-8");
+            //    res.end(content);
+            //});
+        }
+    });
 }
